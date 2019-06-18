303837df29967b124dce77d7be37c5dbef5b4179
deepak <deepak.pednekar009@gmail.com>
2019-06-18 11:51:02 +0530

    ORM with sqlalchemy added

--- flaskr/controller.py
+++ flaskr/controller.py
@@ -1,16 +1,38 @@
 from flask import Blueprint, jsonify, request, current_app
 from . import extension
+from . import models
+from .extension import db, log
 
 log = None
 user_bp = Blueprint('user', __name__, url_prefix='/user') 
 
 def user_root():
+    log.info('user_root | request | %s', request)
     if request.method.lower() == 'get':
-
+        data = models.User.query.all()
         d = {'msg': 'Operation Successful GET', 'type':'+OK'}
         return  jsonify(d), 200
-    # log.info('user_root | request | %s', request.get_json())
+    _json = request.get_json() 
+    print('json', _json)
+    if not _json:
+        d = {'type':'-ERR', 'msg':'json not found'}
+        return jsonify(d), 404
+
+    if 'username' not in _json.keys() or 'email' not in _json.keys():
+        d = {'type':'-ERR', 'msg':'username or email not found'}
+        return jsonify(d), 404
+
+    models.User(**_json)
+    db.session.commit()
     d = {'msg': 'Operation Successfull', 'type':'+OK'}
     return  jsonify(d), 200
+
+def sendResponse(success=False, msg=''):
+    d = {}
+    d['msg'] = msg
+    code = 401
+    if success:
+        code=200
+    return jsonify(d) 
 
 user_bp.add_url_rule('', '', user_root, methods=('GET', 'POST'))
--- flaskr/models.py
+++ flaskr/models.py
@@ -4,6 +4,7 @@ from .extension import db
 class User(db.Model):
    id = db.Column(db.Integer, primary_key=True) 
    username = db.Column(db.String(80), nullable=False)
+   email = db.Column(db.String(80), nullable=False)
 
    def __repr__(self):
         return '<User {0}>'.format(self.username)
