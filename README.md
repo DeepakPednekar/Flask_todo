3e4ba36533f10f2dbada1219fe7182ad353252a5
deepak <deepak.pednekar009@gmail.com>
2019-06-19 15:00:22 +0530

    updated :- structure updated

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is backend code for TODO app
+
+- this is a rest api using Flask framework in python
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name="FREST_TODO",
+    description="Flask REST API for TODO app",
+    packages=['TodoApp.flaskr'],
+    author="Deepak Pednekar",
+    )
