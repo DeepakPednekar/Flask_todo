59c95ffc90e8ba03d4943f5b2d39fb54d099c28c
deepak <deepak.pednekar009@gmail.com>
2019-06-20 17:41:12 +0530

    migration added

--- TodoApp/flaskr/__init__.py
+++ TodoApp/flaskr/__init__.py
@@ -6,11 +6,15 @@ from .extension import db, log
 
 from . import controller
 
+## for db migrations
+from flask_migrate import Migrate
+
 def create_app(test_config=None):
     app = Flask(__name__)
     db_url = os.path.join(app.instance_path, 'flaskr.sqlite')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.sqlite'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskr.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_url)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.debug=True
     # app.config.from_mapping()
@@ -21,12 +25,14 @@ def create_app(test_config=None):
         pass
 
 
-    db.init_app(app)
     log = app.logger
 
     with app.app_context():
         app.register_blueprint(controller.user_bp)
+        db.init_app(app)
         db.create_all()
+
+        Migrate(app, db)
 
 
     # log.info('blueprints | %s', app.blueprints.get('user').url_prefix)
--- TodoApp/flaskr/models.py
+++ TodoApp/flaskr/models.py
@@ -3,11 +3,12 @@ from .extension import db
 
 class User(db.Model):
    id = db.Column(db.Integer, primary_key=True) 
+   name = db.Column(db.String(20), nullable=True, default='')
    username = db.Column(db.String(80), nullable=False)
    email = db.Column(db.String(80), nullable=False)
 
    def __repr__(self):
         return '<User {0}>'.format(self.username)
-        
+
    def get_json(self):
-        return {'id': self.id, 'username': self.username, 'email': self.email}
+        return {'id': self.id, 'username': self.username, 'email': self.email, 'name':self.name}
